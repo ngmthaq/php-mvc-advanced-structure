031416a533ee7f0ff6655f7ddcf57e2cd514c3ce
ngmthaq <nguyenmanhthang2000.fb@gmail.com>
2022-10-23 19:17:23 +0700

    update tag 0.1.1

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,5 @@ class User extends Model
     public function __construct()
     {
         parent::__construct();
-        $this->hidden = ["password", "remember_token", "reset_password_token"];
     }
 }
--- app/Views/pages/index.blade.php
+++ app/Views/pages/index.blade.php
@@ -6,11 +6,8 @@
 @section('title', 'PHP')
 
 @section('main')
-    <p>Hello {{ $title }}</p>
+
 @endsection
 
 @push('js')
-    <script>
-        console.log(trans("_404"));
-    </script>
 @endpush
